aa6b3ef60e7685924f22f1ecedff5ca583119108
LAPTOP-AKJKNNGF\ASUS <624235057@parichat.skru.ac.th>
2021-10-08 21:21:37 +0700

    first commit

---
+++ src/components/Cart.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <h2>Books in Cart</h2>
+    <div v-bind:key="book.bookid" v-for="book in cart">
+        <CartItem v-bind:book="book" v-on:decrease:cartitem="Decrease" v-on:Increase:cartitem="Increase" v-on:Delete:cartitem="Delete" />
+    </div>
+</div>
+</template>
+
+<script>
+import CartItem from './CartItem.vue';
+export default {
+    name: "Cart",
+    props: ["cart"],
+    components: {
+        CartItem
+    },
+    methods:{
+        Increase(bookId){
+            this.$emit("Increase:cart",bookId);
+        },
+        Decrease(bookId){
+            this.$emit("decrease:cart",bookId);
+        },
+        Delete(book){
+            this.$emit("Delete:cart",book);
+        }
+
+
+    }
+
+}
+</script>
+
+<style scoped>
+.book-item {
+    background: #e6f2ff;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+</style>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="book-item" v-bind:class="{'is-complete':book.completed}">
+
+    <b>{{book.title}}</b><br />
+    <b>Price:</b> {{book.price}}<br />
+    <b>Qty:</b> {{book.qty}}<br />
+    <div align="center">
+        <button class="btn btn-success" v-on:click="Increase(book.bookid)">&nbsp;+&nbsp;</button>&nbsp;
+        <button class="btn btn-info" v-on:click="Decrease(book.bookid)">&nbsp;-&nbsp;</button>&nbsp;
+        <button class="btn btn-danger" v-on:click="Delete(book)">&nbsp;x&nbsp;</button>
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: "CartItem",
+    props: ["book"],
+    methods:{
+        Increase(bookId){
+            this.$emit("Increase:cartitem",bookId);
+        },
+        Decrease(bookId){
+            this.$emit("decrease:cartitem",bookId);
+        },
+        Delete(book){
+            this.$emit("Delete:cartitem",book);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.book-item {
+    background: #e6f7ff;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+</style>
